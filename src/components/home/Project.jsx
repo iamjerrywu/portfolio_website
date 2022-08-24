51e38e11cbf64cccee1b1996c9b54731efcacf53
Jerry Wu <iamjerrywu@gmail.com>
2022-08-25 06:46:10 +0800

    check in customized features

--- src/components/home/Project.jsx
+++ src/components/home/Project.jsx
@@ -14,8 +14,6 @@ const dummyProject = {
   pushed_at: null,
 };
 const API = "https://api.github.com";
-// const gitHubQuery = "/repos?sort=updated&direction=desc";
-// const specficQuerry = "https://api.github.com/repos/hashirshoaeb/";
 
 const Project = ({ heading, username, length, specfic }) => {
   const allReposAPI = `${API}/users/${username}/repos?sort=updated&direction=desc`;
@@ -23,7 +21,6 @@ const Project = ({ heading, username, length, specfic }) => {
   const dummyProjectsArr = new Array(length + specfic.length).fill(
     dummyProject
   );
-
   const [projectsArray, setProjectsArray] = useState([]);
 
   const fetchRepos = useCallback(async () => {
@@ -31,18 +28,12 @@ const Project = ({ heading, username, length, specfic }) => {
     try {
       // getting all repos
       const response = await axios.get(allReposAPI);
-      // slicing to the length
-      repoList = [...response.data.slice(0, length)];
-      // adding specified repos
-      try {
-        for (let repoName of specfic) {
-          const response = await axios.get(`${specficReposAPI}/${repoName}`);
-          repoList.push(response.data);
+      // selecting specific repos only
+      for (let repo of response.data) {
+        if (repo.name == 'FitCheck' || repo.name == 'Publish-Subscribe-Platform' || repo.name == 'MedAnnot' || repo.name == 'Python-Project') {
+          repoList.push(repo);
         }
-      } catch (error) {
-        console.error(error.message);
       }
-      // setting projectArray
       // TODO: remove the duplication.
       setProjectsArray(repoList);
     } catch (error) {
@@ -53,6 +44,7 @@ const Project = ({ heading, username, length, specfic }) => {
   useEffect(() => {
     fetchRepos();
   }, [fetchRepos]);
+
 
   return (
     <Jumbotron fluid id="projects" className="bg-light m-0">
